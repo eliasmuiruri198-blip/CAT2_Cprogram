c53bae285953469cb2ae03be6a0953a528cbc332
eliasmuiruri198-blip <eliasmuiruri198@gmail.com>
2025-11-05 12:40:14 +0300

    Add files via upload

---
+++ CAT2_Q2.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+
+int main() {
+    float hours, wage, grossPay, tax, netPay;
+
+    //prompt the user to enter the number of hours in a week
+    printf("Enter hours worked in a week: ");
+    scanf("%f", &hours);
+    //prompt the user to enter hourly wage
+    printf("Enter hourly wage: ");
+    scanf("%f", &wage);
+
+    // use of loop 
+    if (hours <= 40) {
+        grossPay = hours * wage;
+    } else {
+        float overtime = hours - 40;
+        grossPay = (40 * wage) + (overtime * wage * 1.5);
+    }
+
+    // Calculate tax
+    if (grossPay <= 600) {
+        tax = grossPay * 0.15;
+    } else {
+        tax = (600 * 0.15) + ((grossPay - 600) * 0.20);
+    }
+
+    // Calculate net pay
+    netPay = grossPay - tax;
+
+    // display the results
+    printf("\nGross Pay: Ksh%.2f", grossPay);
+    printf("\nTaxes: Ksh%.2f", tax);
+    printf("\nNet Pay: Ksh%.2f\n", netPay);
+
+    return 0;
+}
